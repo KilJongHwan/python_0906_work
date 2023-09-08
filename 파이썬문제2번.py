3a16949a5f7ac7ffb9ff6f45d615e35f494eede2
JH <dafr47@naver.com>
2023-09-08 11:11:47 +0900

    회원정보, 문제 수정 추가

--- 파이썬문제2번.py
+++ 파이썬문제2번.py
@@ -8,10 +8,49 @@ n2 = int(input())
 n3 = int(input())
 mul = n1 * n2 * n3
 result = str(mul)
-print(result)
-
 for i in range(ord("0"), ord("9") + 1):
     print(result.count(chr(i)))
 
+# a,b,c = map(int,input().split())
+# total_val = str(a * b * c)
+# for i in range(10):
+#     print(total_val.count(str(i)))
 
+# 영식 요금제는 30초마다 10원씩 청구된다. 이 말은 만약 29초 또는 그 보다 적은
+# 시간 통화를 했으면 10원이 청구된다.
+# 만약 30초부터 59초 사이로 통화를 했으면 20원이 청구된다.
+# 민식 요금제는 60초마다 15원씩 청구된다. 이 말은 만약 59초 또는 그 보다 적은 시간 통화를 했으면
+# 15원이 청구된다. 만약 60초부터 119초 사이로 통화를 했으면 30원이 청구된다.
+# 동호가 저번 달에 새악대로 T 통신사를 이용할 때 통화 시간 목록이 주어지면
+# 어느 요금제를 사용 하는 것이 저렴한지 출력하는 프로그램을 작성하시오.
+# 영식은 Y로, 민식은 M으로 출력한다.
 
+# n = int(input())
+# ySum = mSum = 0
+#
+# call = list(map(int, input().split()))
+# for i in range(n):
+#     ySum += (call[i] // 30) * 10 + 10
+#     mSum += (call[i] // 60) * 15 + 15
+#
+# Y = 30
+# M = 60
+# for i in range(n):
+#     t = int(input())
+#     if t - Y > 0 : ySum += 10 * (t / Y)
+#     if t - M > 0 : mSum += 15 * (t / M)
+#     ySum += 10
+#     mSum += 15
+#
+# if ySum > mSum : print(f"M {int(mSum)}")
+# elif ySum < mSum: print(f"Y {int(ySum)}")
+# else: print(f"YM {int(ySum)},{int(mSum)}")
+
+# 대소문자 바꾸기
+# rst = ""
+# for e in input():   # 입력 받는 문자열 만큼 자동 출력
+#     if e.islower():
+#         rst += e.upper()
+#     else:
+#         rst += e.lower()
+# print(rst)
--- 회원정보출력하기예제.py
+++ 회원정보출력하기예제.py
@@ -1,24 +1,55 @@
 # 회원정보를 입력 받아서 출력 하는 예제 진행
-#
 # - 이름 입력
 # - 나이 입력 : 1 ~ 199까지 입력 받고 잘못된 값이 오면 재 입력 요청을 한다.
 # - 성별 입력 : 영문자 (M과m은 남성) (F와 f는 여성)으로 입력 받고 나머지는 재 입력 요청을 한다.
 # - 직업 입력 : 1(학생), 2(회사원), 3(주부), 4(무직)으로 입력 받고 나머지는 재 입력 요청 한다.
 # - 결과는 마지막에 한번에 출력 한다.
+def input_age():
+    while True:
+        age = input("나이를 입력 하세요 : ")
+        if age.isdigit():
+            age = int(age)
+            if 0 < age < 200: return age
+        print("회원정보를 잘못 입력하셧습니다 다시 입력해주세요")
 
-name = input("이름을 입력하세요 : ")
+
+def input_sex():
+    while True:
+        sex = input("성별을 입력 하세요 : ")
+        if sex == "m" or "M":
+            return "male"
+        elif sex == "f" or "F":
+            return "female"
+        else:
+            print("회원정보를 잘못 입력하셧습니다 다시 입력해주세요")
+
+
+def input_jobs():
+    while True:
+        job = input("직업을 입력 하세요 : ")
+        if job.isdigit():
+            job = int(job)
+            if 0 < job < 5:
+                return job
+            else:
+                print("회원정보를 잘못 입력하셧습니다 다시 입력해주세요")
+
+
+def print_info(name,age,sex,job):
+    job_str = "", "학생", "회사원", "주부", "무직"
+    print("=" * 3, "회원 정보", "=" * 3)
+    return  f" 이름 : {name} 나이 : {age} 성별 : {sex} 직업 : {job_str[job]}"
+
+
+# 함수는 선언 이후 호출해야 동작
+member_info = "member.txt"
+fd = open(member_info, "wt",encoding="utf-8")
 while True:
-    age = int(input("니이를 입력 하세요 : "))
-    sex = input("성별을 입력 하세요 : ")
-    if 0 < age < 200 and (sex == "m" or "M" or "f" or "F"):
-        break
-    else:
-        print("회원정보를 잘못 입력 하셨습니다 다시 입력해주세요")
-job = int(input("직업을 입력 하세요 [1]학생 [2]회사원 [3]주부 [4]무직 : "))
-if job == 1: job = "학생"
-elif job == 2: job = "회사원"
-elif job == 3: job = "주부"
-else:job = "무직"
-
-print(f"이름 : {name} 나이 : {age} 성별 : {sex} 직업 : {job}")
-
+    name = input("이름을 입력하세요 : ")
+    if name == "quit" : break
+    age = input_age()
+    sex = input_sex()
+    job = input_jobs()
+    rst = print_info(name,age,sex,job)
+    fd.write(rst + "\n")
+fd.close()
