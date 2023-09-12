f625c9c54013bff3222771c8b3f960ea7622dcf2
JH <dafr47@naver.com>
2023-09-12 09:32:51 +0900

    평균은 넘겠지 문제 추가

--- 평균은넘겠지문제.py
+++ 평균은넘겠지문제.py
@@ -1,16 +1,31 @@
-def get_avr():
-    avr = [0, ]
-    n = 0
-    T = int(input())
-    for i in range(T):
-        alist = list(map(int, input().split(" ")))
-        avr[n] = sum(alist) / len(alist)
-        if len(alist) >= T: n += 1
-    return avr
+# 내가 푼 문제
+def avr():
+    cnt = 0
+    array_list = list(map(int, input().split()))
+    for e in array_list:
+        if (sum(array_list) - array_list[0]) / (len(array_list) - 1) < e:
+            cnt += 1
+    average = cnt / (len(array_list) - 1) * 100
+    return average
 
-
-avr_list = [0, ]
 C = int(input())
+alist = []
 for i in range(C):
-    avr_list = get_avr()
-print(avr_list)
+    alist.append(avr())
+for e in alist:
+    print(f"{e:.3f}%")
+
+# def over_rate():  # 각 케이스에서 평균이 넘는 비율 구하기
+#     ls = list(map(int, input().split()))  # 공백기준으로 입력 받아서 정수형 리스트로 받기
+#     average = sum(ls[1:]) / len(ls[1:])  # list 멘앞의 요소 제외한 나머지 평균
+#     cnt = 0  # 평균이 넘는 인원
+#     for i in range(1, len(ls)):
+#         if ls[i] > average : cnt += 1
+#     return cnt / (len(ls) - 1) * 100  # 백분율 표기로 변경
+#
+# n = int(input()) # 총 테스트 개수
+# rst = []    # 각 케이스에 대한 결과값 list
+# for i in range(n):  # 총 테스트 개수 만큼 반복
+#     rst.append(over_rate())
+# for e in rst:
+#     print(f"{e:.3f}%")
