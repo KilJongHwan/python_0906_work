ab768a4eaac562a1fd75d866ed927f6b0eb81afb
JH <dafr47@naver.com>
2023-09-13 10:40:04 +0900

    모듈, flask 추가

---
+++ 플라스크.py
@@ -0,0 +1,14 @@
+# 플라스크 : 웹개발을 위한 라이브러리 모듈
+# Django는 매우 다양한 기능을 제공하는 웹 개발 프레임워크
+# Flask는 웹 어플리케이션 개발을 위한 간결하고 유연한 도구
+# Flask는 마이크로 프레임워크 , 간단한 핵심 기능을 제공하면서도 확장성 유연성을 갖추고있다.
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, Flask"
+
+if __name__ == "__main__":
+    app.run()
