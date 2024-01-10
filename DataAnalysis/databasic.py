f7e2044bafb854eaabce110809b978457c162542
JH <dafr47@naver.com>
2024-01-10 12:20:26 +0900

    데이터분석 추가

--- DataAnalysis/databasic.py
+++ DataAnalysis/databasic.py
@@ -69,3 +69,62 @@ print(a16)
 # 지정한 범위에 해당하는 정수로 난수 배열 생성
 a17 = np.random.randint(10, size=(3,4)) # 0 ~ 10 미만의 정수
 print(a17)
+
+# 배열의 연산 : 넘파이의 배열은 배열의 형태가 같다면 사칙 연산 수행 가능
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+result = arr1 ** arr2
+print(result)
+
+# 요소별 연산 : true false 반환
+arr3 = np.array([10,20,30,40,50])
+print(arr3 > 20)
+
+# 통계를 위한 연산 : 배열의 합, 평균 표준편차, 분산, 최소값, 최대값, 누적합, 누적통, 등의 통계에서 많이 사용되는 메소드
+arr4 = np.arange(5) # 0 ~ 5 미만 배열
+print(f"합계 {arr4.sum()}")
+print(f"평균 {arr4.mean()}")
+print(f"표준편차 {arr4.std()}")
+print(f"분산 {arr4.var()}")
+print(f"최소값 {arr4.min()}")
+print(f"최대값 {arr4.max()}")
+
+# 배열 인덱싱
+arr5 = np.arange(1,6)
+print(arr5[3])
+# 슬라이싱
+new_arr = arr5[1:3] # 1번인덱스에서 3번 인덱스 미만의 값을 슬라이싱
+print(new_arr)
+
+# Universal 함수 : 배열의 원소별 연산을 지원하는 함수
+# 산술연산 : add(), substract(), multiply() ,divide() ,power()
+# 삼각함수 : sin(), cos(), tan()
+# 지수와 로그 : exp(), log(), log10()
+# 집계함수 : sum(), mean(), max(), min()
+# 논리함수 : logical_and(), logical_or(), logical_not()
+
+xx = np.arange(0., 2*np.pi, 0.1)
+yy = np.sin(xx)
+print(yy)
+
+matrix1 = np.array([[1,2], [4,5]])
+metrix2 = np.array([[5,6], [7,8]])
+# 행렬 덧셈
+res = np.add(matrix1, metrix2)
+print(res)
+# 정렬과 탐색 : 배열의 정력과 탐색을 위한 함수의 메소드
+xxx = np.array([9,8,7,2,3,4,6,11])
+print(np.amin(xxx)) # 배열 내에 있는 최소값
+print(np.amax(xxx)) # 배열 내에 있는 최대값
+print(np.argmin(xxx)) # 배열 내의 최소값의 위치
+print(np.sort(xxx))
+print(np.argsort(xxx)) # 값의 인덱스
+
+# 크기가 다른 배열 생성
+ax = np.array([1, 2, 3]) # 1차원 배열
+bx = np.array([[4], [5], [6]]) # 2차원 배열 (3 X 1)
+
+# 브로드캐스팅을 사용한 배열 연산
+c = ax + bx
+
+print(c)
