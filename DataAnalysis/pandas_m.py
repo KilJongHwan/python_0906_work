8d7bc3efb5b930bb44221b31c1142a2d5fca002a
JH <dafr47@naver.com>
2024-01-10 14:32:37 +0900

    데이터분석 pandas 추가

--- DataAnalysis/pandas_m.py
+++ DataAnalysis/pandas_m.py
@@ -29,3 +29,37 @@ print(s5)
 # 날짜 자동 생성 : date_range
 s6 = pd.date_range(start='2023-09-16', end='2023-10-10')
 print(s6)
+
+# 달력을 요일 기준으로 일주일씩 증가
+s7 = pd.date_range(start='2023-09-16', periods=4, freq='W')
+print(s7)
+
+# 데이터 프레임 : 데이터를 다룰 때 가장 많이 사용하는 데이터 형태, 행과 열로 구성된 사각형 모양의 표
+df = pd.DataFrame({'name' : ['james', 'kane', 'justice','cry','samuel'],
+                   'kor' : [90, 88, 97, 77, 92],
+                   'eng': [80,92,78,95,76]
+                   })
+print(df)
+print(df['kor'])
+print(sum(df['kor']/df['kor'].size)) # 평균 구하기
+
+# 실습문제
+df_fruit = pd.DataFrame({
+    '제품' : ['apple', 'strawberry', 'watermelon'],
+    '가격' : [1800, 1500, 3000],
+    '판매량' : [24, 38, 13]
+})
+print(f"Average Price : {sum(df_fruit['가격']) / df_fruit['가격'].size}")
+print(f"Average Solved Price : {sum(df_fruit['판매량']) / df_fruit['판매량'].size}")
+
+# Excel 파일 읽기
+df_excel = pd.read_excel('./excel_exam.xlsx')
+print(df_excel)
+print(f"Exel eng Average grade : {sum(df_excel['english']) / df_excel['english'].size}")
+
+# CSV(Comma Seperated Values) 파일 읽기
+df_csv = pd.read_csv('./exam.csv')
+print(df_csv)
+
+# 데이터 프레임을 CSV 파일로 저장하기
+df_fruit.to_csv('FruitDataFrame.csv', index=False)
